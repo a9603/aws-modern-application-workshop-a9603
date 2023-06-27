961ecbabb6e2abd6cf5977316b13e427aefb87d3
a9603 <a9603@live.co.kr>
2023-06-27 04:26:41 +0000

    inprogress module2

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -2,6 +2,8 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { WebApplicationStack } from "../lib/web-application-stack";
+import { NetworkStack } from "../lib/network-stack";
 
 const app = new cdk.App();
 new WebApplicationStack(app, "MythicalMysfits-Website");
+const networkStack = new NetworkStack(app, "MythicalMysfits-Network");
